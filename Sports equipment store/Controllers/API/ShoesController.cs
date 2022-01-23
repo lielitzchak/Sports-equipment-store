efa6446b937d86e1686fdc499745e50c6e0e6a5e
liel itzchak <liel.izcha321@gmail.com>
2022-01-23 20:33:05 +0200

    shes part

---
+++ Sports equipment store/Controllers/API/ShoesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sports_equipment_store.Models;
+
+namespace Sports_equipment_store.Controllers.API
+{
+    public class ShoesController : Controller
+    {
+        string stringConnection = "Data Source=LIEL-ITZCHAK;Initial Catalog=SportsEquipmentSoreDB;Integrated Security=True;Pooling=False";
+        ShoesDataContextDataContext dataContext = new ShoesDataContextDataContext();
+        // GET: Shoes
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult ShowShoes()
+        {
+            try
+            {
+                List<Shoe> Shoes = dataContext.Shoes.ToList();
+
+                return View(Shoes);
+
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+
+
+
+        }
+
+    }
+}
